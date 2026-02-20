8b87611a0cd583702b12c34d3603f6e71894df96
IRSPROJECTS <ckarounias@innovation-res.eu>
2026-02-20 11:30:18 +0200

    add create tabls

--- app.py
+++ app.py
@@ -26,14 +26,15 @@ bcrypt = Bcrypt(app)
 # MODEL
 # =====================
 
-with app.app_context():
-    db.create_all()
-    
+
+
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(200), nullable=False)
-
+    
+with app.app_context():
+    db.create_all()
 
 # =====================
 # ROUTES
